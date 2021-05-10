29476f29dc7a37f3344ed445fbd9a8a7c55e6bd7
madmath03 <mb.mathieu.brunot@gmail.com>
2021-05-10 16:35:53 +0000

    🚧 Improve LDAP Client
    Signed-off-by: madmath03 <mb.mathieu.brunot@gmail.com>

--- app/src/Service/Ldap/Client.php
+++ app/src/Service/Ldap/Client.php
@@ -2,6 +2,7 @@
 
 namespace App\Service\Ldap;
 
+use Symfony\Component\Ldap\Adapter\QueryInterface;
 use Symfony\Component\Ldap\Entry;
 use Symfony\Component\Ldap\Exception\LdapException;
 use Symfony\Component\Ldap\Ldap;
@@ -73,7 +74,12 @@ class Client
         }
 
         $this->ldap->bind($fullDn, $password);
-        $result = $this->ldap->query($fullDn, $query)->execute()[0];
+
+        $results = $this->ldap->query($fullDn, $query)->execute();
+        // TODO Do not bind in executeQuery
+        //$results = $this->executeQuery($query, $fullDn);
+
+        $result = $results[0];
 
         return $result;
     }
@@ -85,12 +91,18 @@ class Client
      *
      * @$ldappsalm-return \Symfony\Component\Ldap\Adapter\CollectionInterface|array<array-key, Entry>
      */
-    public function executeQuery(string $query)
+    private function executeQuery(string $query, string $base_dn = null, array $options = [])
     {
+        // TODO Do not bind in executeQuery (must be done only once before executing query)
         $this->ldap->bind($this->config['search_dn'], $this->config['search_password']);
-        return $this->ldap->query($this->config['base_dn'], $query)->execute();
+
+        if (empty($base_dn)) {
+            $base_dn = $this->config['base_dn'];
+        }
+
+        return $this->ldap->query($base_dn, $query, $options)->execute();
     }
-    
+
     /**
      * @return bool
      *
@@ -138,21 +150,26 @@ class Client
     }
 
     /**
+     * Delete an entry from a directory.
+     *
      * @return bool
      *
      * @throws LdapException
      */
     public function delete(string $fullDn)
     {
+        // TODO Do not bind here (must be done only once before executing query)
         $this->ldap->bind($this->config['search_dn'], $this->config['search_password']);
+
+        // Removing an existing entry
         $entryManager = $this->ldap->getEntryManager();
         $entryManager->remove(new Entry($fullDn));
-        // Removing an existing entry
+
         return true;
     }
 
     /**
-     * @var string
+     * Search LDAP tree.
      *
      * @return Entry[]|\Symfony\Component\Ldap\Adapter\CollectionInterface
      *
@@ -160,11 +177,39 @@ class Client
      *
      * @psalm-return \Symfony\Component\Ldap\Adapter\CollectionInterface|array<array-key, Entry>
      */
-    public function search(string $query)
+    public function search(string $query, string $base_dn = null, array $options = ['scope' => QueryInterface::SCOPE_SUB])
     {
-        //Symfony base function for fetching ldap
-        // TODO Do not bind inside search (must be done before)
-        $entries = $this->executeQuery($query);
-        return $entries;
+        $search_options = array_merge(['scope' => QueryInterface::SCOPE_SUB], $options);
+        return $this->executeQuery($query, $base_dn, $search_options);
+    }
+
+    /**
+     * Single-level search.
+     *
+     * @return Entry[]|\Symfony\Component\Ldap\Adapter\CollectionInterface
+     *
+     * @throws LdapException
+     *
+     * @psalm-return \Symfony\Component\Ldap\Adapter\CollectionInterface|array<array-key, Entry>
+     */
+    public function list(string $query, string $base_dn = null, array $options = [])
+    {
+        $list_options = array_merge(['scope' => QueryInterface::SCOPE_ONE], $options);
+        return $this->executeQuery($query, $base_dn, $list_options);
+    }
+
+    /**
+     * Read an entry.
+     *
+     * @return Entry
+     *
+     * @throws LdapException
+     *
+     * @psalm-return Entry
+     */
+    public function get(string $query, string $base_dn = null, array $options = [])
+    {
+        $get_options = array_merge(['scope' => QueryInterface::SCOPE_BASE], $options);
+        return $this->executeQuery($query, $base_dn, $get_options);
     }
 }
